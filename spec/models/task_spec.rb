a9e7df616200dfc9bbc25c63ae1cef689d800c02
Daniel <ardekanicapital@gmail.com>
2019-08-13 21:31:42 -0400

    inital commit and rspec tests

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe "#toggle_complete!" do
+    it "should switch complete to false if it was true" do
+      task = Task.create(complete: true)
+      task.toggle_complete!
+      expect(task.complete).to eq(false)
+    end
+
+    it "should switch complete to true if it was false" do
+      task = Task.create(complete: false)
+      task.toggle_complete!
+      expect(task.complete).to eq(true)
+    end
+
+    it "If task is overdue then display true" do 
+      take = Task.create(overdue?: false)
+      task.time.overdue?
+      expect(task.overdue?).to eq(true)
+    end 
+  end
+end
